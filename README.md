a5b7ded6bfc45d005ae6c9ac339bd1eea7064fdd
GaryHU <swe1609011@gmail.com>
2023-04-03 16:53:19 +0800

    update aig conversion support

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # pybmc
 
+## Dependencies
+
+* [aigertool](https://github.com/arminbiere/aiger): Used to convert `aig` format to `aag` format
 ## USAGE
 
 * `python main.py --aag <path to config file> --k <number of bound>`: run the algorithm with the given aag format file and bound k
--- main.py
+++ main.py
@@ -1,37 +1,39 @@
 '''
 Main function to run BMC
 '''
-
-
-
-
-
-
+import subprocess
 import argparse
-import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+import os, os.path, shutil
 from datetime import datetime
 from datetime import timedelta
 from multiprocessing import Process
 from threading import Thread
-
 from time import sleep
 import sys
-sys.path.append("..")
+from pathlib import Path
 import model
 import bmc
 import time
 import argparse
 import z3
+import tempfile
 
+def convert_aig_to_aag(file):
+    aig_path = file
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        aigtoaig_cmd='./aiger/aigtoaig'
+        tmpdir = Path(tmpdirname)
+        shutil.copy(aig_path, tmpdir)
+        subprocess.call([aigtoaig_cmd, aig_path, tmpdir / "tmp.aag"])
+        return bmc.BMC(*m.parse(tmpdir / "tmp.aag"))
+        
 
-#TODO: add a switch to open "generate training set or not"
 if __name__ == '__main__':
     help_info = "Usage: python main.py <file-name>.aag"
     parser = argparse.ArgumentParser(description="Run tests examples on the PDR algorithm")
     parser.add_argument('--aag', type=str, help='The name of the test to run', default=None, nargs='?')
     parser.add_argument('--k', type=int, help='The number of unrolling steps', default=10, nargs='?')
-    args = parser.parse_args()
+    args = parser.parse_args(['--aag', '/data/guangyuh/coding_env/pybmc/dataset/aig_benchmark/hwmcc10-mod/bob2.aig', '--k', '10'])
     m = model.Model()
     # SAT 1 - simple
     #file = "/data/guangyuh/coding_env/pybmc/dataset/aig_benchmark/hwmcc07_tip_aag/texas.ifetch1^8.E.aag"
@@ -41,8 +43,10 @@ if __name__ == '__main__':
     # file = "/data/guangyuh/coding_env/pybmc/dataset/aig_benchmark/hwmcc07_tip/nusmv.syncarb5^2.B.aag"
 
     file = args.aag
-    prev_fidx = 0
-    bmc = bmc.BMC(*m.parse(file))
+    if file.endswith(".aig"): 
+        bmc = convert_aig_to_aag(file)
+    else:
+        bmc = bmc.BMC(*m.parse(file))
 
     bmc.setup()
 
